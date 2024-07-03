352bb38e36b3d38ae7dcce3fd639347e78aa32c2
returnaction <obergannv@gmail.com>
2024-07-03 12:57:11 -0400

    Add UnitOfWork

---
+++ Сантехник.Repository/UnitOfWork/Abstract/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Сантехник.CoreLayer.BaseEntities;
+using Сантехник.RepositoryLayer.Repositories.Abstract;
+
+namespace Сантехник.RepositoryLayer.UnitOfWork.Abstract
+{
+    public interface IUnitOfWork
+    {
+        void Commit();
+        Task CommitAsync();
+        IGenericRepositories<T> GetGeneric<T>() where T : class, IBaseEntity, new();
+        ValueTask DisposeAsync();
+    }
+}
---
+++ Сантехник.Repository/UnitOfWork/Concrete/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Сантехник.RepositoryLayer.Context;
+using Сантехник.RepositoryLayer.Repositories.Abstract;
+using Сантехник.RepositoryLayer.Repositories.Concrete;
+using Сантехник.RepositoryLayer.UnitOfWork.Abstract;
+
+namespace Сантехник.RepositoryLayer.UnitOfWork.Concrete
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            return _context.DisposeAsync(); // или сделать not async
+        }
+
+        IGenericRepositories<T> IUnitOfWork.GetGeneric<T>()
+        {
+            return new GenericRepositories<T>(_context);
+        }
+    }
+}
