37bd11c588809e1af666ebda005cc45d584d0dee
returnaction <obergannv@gmail.com>
2024-07-03 16:37:46 -0400

    Add automapper to servicelayer

---
+++ Сантехник.ServiceLayer/Extensions/ServiceLayerExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сантехник.ServiceLayer.Extensions
+{
+    public static class ServiceLayerExtensions
+    {
+        public static IServiceCollection LoadServiceLayerExtensions(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            return services;
+        }
+
+    }
+}
