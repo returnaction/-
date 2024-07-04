7ea60aa9e2ff2cbbe64d64567ca3b736b544d8ca
returnaction <obergannv@gmail.com>
2024-07-04 14:25:14 -0400

    Completed ServiceService

--- Сантехник.ServiceLayer/Services/Abstract/IServiceService.cs
+++ Сантехник.ServiceLayer/Services/Abstract/IServiceService.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Сантехник.EntityLayer.WebApplication.ViewModels.ServiceVM;
 
 namespace Сантехник.ServiceLayer.Services.Abstract
 {
     public interface IServiceService
     {
+        Task AddCategoryService(ServiceAddVM request);
+        Task DeleteCategoryAsync(int id);
+        Task<List<ServiceListVM>> GetAllListAsync();
+        Task<ServiceUpdateVM> GetCategoryById(int id);
+        Task UpdateCategoryAsync(ServiceUpdateVM request);
     }
 }
--- Сантехник.ServiceLayer/Services/Concrete/ServiceService.cs
+++ Сантехник.ServiceLayer/Services/Concrete/ServiceService.cs
@@ -1,13 +1,66 @@
-﻿using System;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Сантехник.EntityLayer.WebApplication.Entities;
+using Сантехник.EntityLayer.WebApplication.ViewModels.CategoryVM;
+using Сантехник.EntityLayer.WebApplication.ViewModels.ServiceVM;
+using Сантехник.RepositoryLayer.Repositories.Abstract;
+using Сантехник.RepositoryLayer.UnitOfWork.Abstract;
 using Сантехник.ServiceLayer.Services.Abstract;
 
 namespace Сантехник.ServiceLayer.Services.Concrete
 {
     public class ServiceService : IServiceService
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly IGenericRepositories<Service> _repository;
+
+        public ServiceService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _repository = _unitOfWork.GetGenericRepository<Service>();
+        }
+
+
+
+        public async Task<List<ServiceListVM>> GetAllListAsync()
+        {
+            List<ServiceListVM>? serviceListVM = await _repository.GetAllEntityList().ProjectTo<ServiceListVM>(_mapper.ConfigurationProvider).ToListAsync();
+            return serviceListVM;
+        }
+
+        public async Task<ServiceUpdateVM> GetCategoryById(int id)
+        {
+            ServiceUpdateVM? service = await _repository.Where(x => x.Id == id).ProjectTo<ServiceUpdateVM>(_mapper.ConfigurationProvider).SingleAsync();
+            return service;
+        }
+
+        public async Task AddCategoryService(ServiceAddVM request)
+        {
+            Service? service = _mapper.Map<Service>(request);
+            await _repository.AddEntityAsync(service);
+            await _unitOfWork.CommitAsync();
+        }
+
+        public async Task UpdateCategoryAsync(ServiceUpdateVM request)
+        {
+            Service? service = _mapper.Map<Service>(request);
+            _repository.UpdateEntity(service);
+            await _unitOfWork.CommitAsync();
+        }
+
+        public async Task DeleteCategoryAsync(int id)
+        {
+            Service? service = await _repository.GetEntityByIdAsync(id);
+            _repository.DeleteEntity(service);
+            await _unitOfWork.CommitAsync();
+        }
     }
 }
