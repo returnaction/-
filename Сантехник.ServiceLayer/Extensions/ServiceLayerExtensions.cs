6a98438574f928d1f333348fcf4523ac47bd4c4f
returnaction <obergannv@gmail.com>
2024-07-05 14:25:14 -0400

    Register services

--- Сантехник.ServiceLayer/Extensions/ServiceLayerExtensions.cs
+++ Сантехник.ServiceLayer/Extensions/ServiceLayerExtensions.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Сантехник.ServiceLayer.Services.Abstract;
+using Сантехник.ServiceLayer.Services.Concrete;
 
 namespace Сантехник.ServiceLayer.Extensions
 {
@@ -13,6 +15,20 @@ namespace Сантехник.ServiceLayer.Extensions
         public static IServiceCollection LoadServiceLayerExtensions(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            //services.AddScoped<IAboutService, AboutService>(); silly way to add services.
+
+            var types = Assembly.GetExecutingAssembly().GetTypes().Where(x => x.IsClass && !x.IsAbstract && x.Name.EndsWith("Service"));
+
+            foreach (var serviceType in types)
+            {
+                var iServiceType = serviceType.GetInterfaces().FirstOrDefault(x => x.Name == $"I{serviceType.Name}");
+
+                if(iServiceType != null)
+                {
+                    services.AddScoped(iServiceType, serviceType);
+                }
+            }
             return services;
         }
 
