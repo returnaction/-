86a0e4925f0b4c6c44d584d3adcc9fb5520d0a4b
returnaction <obergannv@gmail.com>
2024-07-05 19:12:50 -0400

    Add AboutAdd functionality

--- Сантехник.EntityLayer/WebApplication/ViewModels/AboutVM/AboutAddVM.cs
+++ Сантехник.EntityLayer/WebApplication/ViewModels/AboutVM/AboutAddVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,8 @@ namespace Сантехник.EntityLayer.WebApplication.ViewModels.AboutVM
 
         public string FileName { get; set; } = null!;
         public string FileType { get; set; } = null!;
+
+        public IFormFile Photo { get; set; } = null!;
 
         public int SocialMediaId { get; set; }
         public SocialMediaAddVM SocialMedia { get; set; } = null!;
