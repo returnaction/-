2c51f8290f3ab0dbda99bab283f330e734d959a0
returnaction <obergannv@gmail.com>
2024-07-06 07:52:28 -0400

    Add Service Controller

---
+++ Сантехник/Areas/Admin/Controllers/ServiceController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Сантехник.EntityLayer.WebApplication.Entities;
+using Сантехник.EntityLayer.WebApplication.ViewModels.ServiceVM;
+using Сантехник.ServiceLayer.Services.Abstract;
+
+namespace Сантехник.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ServiceController : Controller
+    {
+        private readonly IServiceService _serviceService;
+
+        public ServiceController(IServiceService serviceService)
+        {
+            _serviceService = serviceService;
+        }
+
+        
+        public async Task<IActionResult> GetServiceList()
+        {
+            var serviceList = await _serviceService.GetAllListAsync();
+            return View(serviceList);
+        }
+
+        public IActionResult AddService()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddService(ServiceAddVM request)
+        {
+            await _serviceService.AddServiceService(request);
+            return RedirectToAction("GetServiceList", "Service", new { Area = ("Admin") });
+        }
+
+        public async Task<IActionResult> UpdateService(int id)
+        {
+            var service = await _serviceService.GetServiceById(id);
+            return View(service);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateService(ServiceUpdateVM request)
+        {
+            await _serviceService.UpdateServiceAsync(request);
+            return RedirectToAction("GetServiceList", "Service", new { Area = ("Admin") });
+        }
+
+        public async Task<IActionResult> DeleteService(int id)
+        {
+            await _serviceService.DeleteServiceAsync(id);
+            return RedirectToAction("GetServiceList", "Service", new { Area = ("Admin") });
+        }
+    }
+}
