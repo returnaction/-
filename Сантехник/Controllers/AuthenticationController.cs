cf056ccee9e3e1d460212cc4902985d3e93321b3
returnaction <obergannv@gmail.com>
2024-07-07 09:56:49 -0400

    Add SignUoVM class

---
+++ Сантехник/Controllers/AuthenticationController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Сантехник.EntityLayer.Identity;
+using Сантехник.EntityLayer.Identity.ViewModels;
+
+namespace Сантехник.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AuthenticationController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public Task<IActionResult> SignUp(SignUpVM request)
+        //{
+        //    return View();
+        //}
+    }
+}
