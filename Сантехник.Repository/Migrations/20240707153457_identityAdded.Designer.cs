ef739943f4e32556e33e109a980040c879dcddca
returnaction <obergannv@gmail.com>
2024-07-07 11:35:29 -0400

    Add migration new with identity

--- Сантехник.Repository/Migrations/20240707153457_identityAdded.Designer.cs
+++ Сантехник.Repository/Migrations/20240707153457_identityAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Сантехник.RepositoryLayer.Context;
 
@@ -11,9 +12,11 @@ using Сантехник.RepositoryLayer.Context;
 namespace Сантехник.RepositoryLayer.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240707153457_identityAdded")]
+    partial class identityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
