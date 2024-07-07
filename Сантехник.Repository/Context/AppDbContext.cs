5bbd3f1350e6339668687166cdff9551e9e3e464
returnaction <obergannv@gmail.com>
2024-07-07 08:42:27 -0400

    changed dbcontext to IdentityDbContext

--- Сантехник.Repository/Context/AppDbContext.cs
+++ Сантехник.Repository/Context/AppDbContext.cs
@@ -1,15 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Сантехник.EntityLayer.Identity;
 using Сантехник.EntityLayer.WebApplication.Entities;
 
 namespace Сантехник.RepositoryLayer.Context
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<AppUser, AppRole, string>
     {
         public AppDbContext(DbContextOptions options) : base(options)
         {
