ed9960e1a7a588523d8348ccd59064f2bab13aed
returnaction <obergannv@gmail.com>
2024-07-07 12:30:09 -0400

    Add LogIn into AuthenticationController

--- Сантехник/Controllers/AuthenticationController.cs
+++ Сантехник/Controllers/AuthenticationController.cs
@@ -12,13 +12,21 @@ namespace Сантехник.Controllers
     public class AuthenticationController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
         private readonly IValidator<SignUpVM> _signUpValidator;
+        private readonly IValidator<LogInVM> _logInValidator;
+
         private readonly IMapper _iMapper;
 
-        public AuthenticationController(UserManager<AppUser> userManager, IValidator<SignUpVM> signUpValidator, IMapper iMapper)
+        public AuthenticationController(UserManager<AppUser> userManager, IValidator<SignUpVM> signUpValidator, IMapper iMapper, IValidator<LogInVM> logInValidator, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
+
             _signUpValidator = signUpValidator;
+            _logInValidator = logInValidator;
+
             _iMapper = iMapper;
         }
 
@@ -57,9 +65,47 @@ namespace Сантехник.Controllers
         }
 
         [HttpPost]
-        public Task<IActionResult> LogIn(LogInVM request)
+        public async Task<IActionResult> LogIn(LogInVM request, string? returnUrl = null)
         {
+            returnUrl = returnUrl ?? Url.Action("Index", "Dashboard", new { Area = "Admin" });
 
+            var validation = await _logInValidator.ValidateAsync(request);
+            if (!validation.IsValid)
+            {
+                ViewBag.Result = "NotSucceed";
+                validation.AddToModelState(this.ModelState);
+                return View(request);
+            }
+
+            // finding the user
+            var hasUser = await _userManager.FindByEmailAsync(request.Email);
+            if (hasUser == null)
+            {
+                ViewBag.Result = "NotSucceed";
+                ModelState.AddModelErrorList(new List<string> { "Email or Password is worng" });
+                return View(request);
+            }
+
+            // let's signIn
+            var logInResult = _signInManager.PasswordSignInAsync(hasUser, request.Password, request.RememberMe, true);
+
+            if (logInResult.Result.Succeeded)
+            {
+                return Redirect(returnUrl!);
+            }
+
+            // if we didn't succeed;
+
+            if (logInResult.Result.IsLockedOut)
+            {
+                ViewBag.Result = "LockedOut";
+                ModelState.AddModelErrorList(new List<string> { "Your account is locked out for 3 minutes" });
+                return View();
+            }
+
+            ViewBag.Result = "FailedAttempt";
+            ModelState.AddModelErrorList(new List<string> { $"Email or Password is wrong! Failed attempt {await _userManager.GetAccessFailedCountAsync(hasUser)}" });
+            return View();
         }
     }
 }
