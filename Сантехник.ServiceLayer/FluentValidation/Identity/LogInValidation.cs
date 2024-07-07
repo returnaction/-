234f1fdceb10a7f7bf9353eacc5dcd329314accb
returnaction <obergannv@gmail.com>
2024-07-07 12:07:19 -0400

    Add Validation for LogIn

---
+++ Сантехник.ServiceLayer/FluentValidation/Identity/LogInValidation.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Сантехник.EntityLayer.Identity.ViewModels;
+using Сантехник.ServiceLayer.Messages.Identity;
+using Сантехник.ServiceLayer.Messages.WebApplication;
+
+namespace Сантехник.ServiceLayer.FluentValidation.Identity
+{
+    public class LogInValidation : AbstractValidator<LogInVM>
+    {
+        public LogInValidation()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Email"))
+                .Null().WithMessage(ValidationMessages.NullEmptyMessage("Email"))
+                .EmailAddress().WithMessage(IdentityMessages.CheckEmailAddress());
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Password"))
+                .NotNull().WithMessage(ValidationMessages.NullEmptyMessage("Password"));
+            
+        }
+    }
+}
