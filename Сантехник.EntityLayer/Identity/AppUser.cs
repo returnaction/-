7d6a28b256b0bf9e1bdf4d148db2bb352f1649b5
returnaction <obergannv@gmail.com>
2024-07-07 08:37:08 -0400

    add props to app User

--- Сантехник.EntityLayer/Identity/AppUser.cs
+++ Сантехник.EntityLayer/Identity/AppUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace Сантехник.EntityLayer.Identity
 {
-    public class AppUser
+    public class AppUser : IdentityUser
     {
+        public string? FileName { get; set; }
+        public string? FileType { get; set; }
     }
 }
