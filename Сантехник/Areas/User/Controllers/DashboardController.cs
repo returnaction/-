28b24ded157e83de060c8c9687a56b7330020864
returnaction <obergannv@gmail.com>
2024-07-08 16:03:00 -0400

    Add Index Page for User

---
+++ Сантехник/Areas/User/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Сантехник.Areas.User.Controllers
+{
+    [Area("User")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
