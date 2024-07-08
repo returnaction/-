7b222b0a40a51468c4be9d3574e4a317635ee11d
returnaction <obergannv@gmail.com>
2024-07-08 16:35:54 -0400

    Add Mapper for UserEditVM

--- Сантехник.EntityLayer/Identity/ViewModels/UserEditVM.cs
+++ Сантехник.EntityLayer/Identity/ViewModels/UserEditVM.cs
@@ -11,16 +11,16 @@ namespace Сантехник.EntityLayer.Identity.ViewModels
     {
         public string Username { get; set; } = null!;
         public string Email { get; set; } = null!;
-        public string PhoneNumber { get; set; } = null!;
-        public string Password { get; set; } = null!;
-        public string NewPassword { get; set; } = null!;
-        public string ConfirmNewPassword { get; set; } = null!;
+        public string? PhoneNumber { get; set; } 
+        public string? Password { get; set; } 
+        public string? NewPassword { get; set; }
+        public string? ConfirmNewPassword { get; set; }
 
 
-        public string FileName { get; set; } = null!;
-        public string FileType { get; set; } = null!;
+        public string? FileName { get; set; }
+        public string? FileType { get; set; } 
 
-        public IFormFile Photo { get; set; } = null!;
+        public IFormFile? Photo { get; set; } 
 
         public string ConcurrencyStamp { get; set; } = null!;
 
---
+++ Сантехник.ServiceLayer/Automapper/Identity/UserEditMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Сантехник.EntityLayer.Identity;
+using Сантехник.EntityLayer.Identity.ViewModels;
+
+namespace Сантехник.ServiceLayer.Automapper.Identity
+{
+    public class UserEditMapper : Profile
+    {
+        public UserEditMapper()
+        {
+            CreateMap<AppUser, UserEditVM>().ReverseMap();
+        }
+    }
+}
