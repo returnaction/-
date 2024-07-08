34d7af5c2e10dce45d181e884e17ef875bf5379f
returnaction <obergannv@gmail.com>
2024-07-08 16:15:39 -0400

    Add AuthenticationUserController

---
+++ Сантехник.EntityLayer/Identity/ViewModels/UserEditVM.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сантехник.EntityLayer.Identity.ViewModels
+{
+    public class UserEditVM
+    {
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string NewPassword { get; set; } = null!;
+        public string ConfirmNewPassword { get; set; } = null!;
+
+
+        public string FileName { get; set; } = null!;
+        public string FileType { get; set; } = null!;
+
+        public IFormFile Photo { get; set; } = null!;
+
+        public string ConcurrencyStamp { get; set; } = null!;
+
+
+
+    }
+}
---
+++ Сантехник/Areas/User/Controllers/AuthenticationUserController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Сантехник.EntityLayer.Identity;
+
+namespace Сантехник.Areas.User.Controllers
+{
+    [Area("User")]
+    public class AuthenticationUserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public AuthenticationUserController(UserManager<AppUser> userManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> UserEdit()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity!.Name!);
+            var userEditVM = _mapper.Map<UserEditVM>(user);
+
+            return View(userEditVM);
+        }
+    }
+}
