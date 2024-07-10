88ed963810eb50cce77fbb321b89b263fc2e7ec3
returnaction <obergannv@gmail.com>
2024-07-10 13:39:02 -0400

    same

--- Сантехник/Areas/User/Controllers/DashboardController.cs
+++ Сантехник/Areas/User/Controllers/DashboardController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Сантехник.Areas.User.Controllers
 {
-    [Authorize]
+    [Authorize(Roles = "Member, SuperAdmin")]
     [Area("User")]
     public class DashboardController : Controller
     {
