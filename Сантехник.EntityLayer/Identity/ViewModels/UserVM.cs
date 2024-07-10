b63bcf96ce701e72a4ac81cb523c1267cdccaab4
returnaction <obergannv@gmail.com>
2024-07-10 14:22:13 -0400

    add method for admin to get all the users

---
+++ Сантехник.EntityLayer/Identity/ViewModels/UserVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сантехник.EntityLayer.Identity.ViewModels
+{
+    public class UserVM
+    {
+        public string UserName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public List<string>? UserRoles { get; set; } 
+    }
+}
---
+++ Сантехник/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Framework;
+using Microsoft.EntityFrameworkCore;
+using Сантехник.EntityLayer.Identity.Entities;
+using Сантехник.EntityLayer.Identity.ViewModels;
+
+namespace Сантехник.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public AdminController(UserManager<AppUser> userManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> GetUserList()
+        {
+            var userList = await _userManager.Users.ToListAsync();
+            var userListVM = _mapper.Map<List<UserVM>>(userList);
+
+            return View(userListVM);
+        }
+    }
+}
