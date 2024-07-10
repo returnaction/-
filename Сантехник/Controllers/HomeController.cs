4f87bc0375ab8e4b8a5a1faec8ffee101b0789ca
returnaction <obergannv@gmail.com>
2024-07-10 14:37:43 -0400

    add layout for user

--- Сантехник/Controllers/HomeController.cs
+++ Сантехник/Controllers/HomeController.cs
@@ -6,17 +6,14 @@ namespace Сантехник.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+       
         public IActionResult Index()
         {
             return View();
         }
+
+
+
 
         public IActionResult Privacy()
         {
